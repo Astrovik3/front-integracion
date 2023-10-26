b1b587268ba4a00ec8a09fbfd8beb58930a7272c
mfustinana-qontigo <mfustinana@qontigo.com>
2023-10-26 20:16:47 -0300

    Delete fix & battery consumption

--- src/app/robots/CreateRobotForm.js
+++ src/app/robots/CreateRobotForm.js
@@ -13,7 +13,17 @@ function CreateRobotForm({handleCont, setMode}) {
 
     const [name, setName] = useState('');
     const [type, setType] = useState('');
+
     const [battery, setBattery] = useState(null);
+    const handleBattery = (value) =>{
+        setBattery(value < 0 ? 0 : (value > 1 ? 1 : parseFloat(value)));
+    };
+    
+    const [batteryConsumption, setConsumption] = useState(null); 
+    const handleConsumption = (value) =>{
+        setConsumption(value < 0 ? 0 : (value > 1 ? 1 : parseFloat(value)));
+    };
+
     const [velocity, setVelocity] = useState(null);
 
     const [disabled, setDisabled] = useState(true);
@@ -23,6 +33,7 @@ function CreateRobotForm({handleCont, setMode}) {
             name: name,
             type: type,
             battery: battery,
+            batteryConsumption: batteryConsumption,
             velocity: velocity
         };
     }
@@ -40,8 +51,8 @@ function CreateRobotForm({handleCont, setMode}) {
     }
 
     useEffect(() => {
-        setDisabled(name === '' || type === '' || !battery || !velocity)
-    },[name, type, battery, velocity]);
+        setDisabled(name === '' || type === '' || !battery || !batteryConsumption || !velocity)
+    },[name, type, battery, batteryConsumption, velocity]);
 
     return (
         <div className='flex flex-col'>
@@ -72,8 +83,11 @@ function CreateRobotForm({handleCont, setMode}) {
             <label className='block mb-3 font-[500]'>Type</label>
             <input className='w-full h-8 mb-4 rounded outline outline-[1px] outline-gray-400 outline-offset-4 p-2' type='text' name='typeRobot' value={type} onChange={e=>{setType(e.target.value)}}/>
 
-            <label className='block mb-3 font-[500]'>Battery</label>
-            <input className='w-full h-8 mb-4 rounded outline outline-[1px] outline-gray-400 outline-offset-4 p-2' type='number' name='batteryRobot' value={battery} onChange={e=>{setBattery(e.target.value)}}/>
+            <label className='block mb-3 font-[500]'>Battery (Entre 0 y 1)</label>
+            <input className='w-full h-8 mb-4 rounded outline outline-[1px] outline-gray-400 outline-offset-4 p-2' type='number' name='batteryRobot' value={battery} onChange={e=>{handleBattery(e.target.value)}}/>
+
+            <label className='block mb-3 font-[500]'>Battery Consuption (Entre 0 y 1)</label>
+            <input className='w-full h-8 mb-4 rounded outline outline-[1px] outline-gray-400 outline-offset-4 p-2' type='number' name='batteryConsumptionRobot' value={batteryConsumption} onChange={e=>{handleConsumption(e.target.value)}}/>
 
             <label className='block mb-3 font-[500]'>Velocity (m/s)</label>
             <input className='w-full h-8 mb-6 rounded outline outline-[1px] outline-gray-400 outline-offset-4 p-2' type='number' name='velocityRobot' value={velocity} onChange={e=>{setVelocity(e.target.value)}}/>
